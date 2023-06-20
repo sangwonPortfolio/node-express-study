020aa912e4751c52af778535a341922d267c3c09
choisangwon <rngus3698@naver.com>
2023-06-20 18:44:44 +0900

    불필요한 파일 정리 및 추가

--- src/config/jwtConfig.js
+++
@@ -1,7 +0,0 @@
-const jwtConfig = () => {
-  return {
-    SECRETKEY: process.env.JWT_SECRET_KEY,
-  }
-}
-
-module.exports = jwtConfig;
--- src/utils/database/getConnection.js
+++ src/utils/database/getConnection.js
@@ -1,7 +1,16 @@
-const dbConfig = require("../../config/dbConfig");
 const mysql = require("mysql");
 
-const pool = mysql.createPool(dbConfig());
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  connectionLimit: process.env.DB_CONNECTIONLIMIT,
+  timezone: process.env.DB_TIMEZONE,
+  charset: process.env.DB_CHARSET,
+  multipleStatements: process.env.DB_MULTIPLESTATEMENTS
+});
 
 const getConnection = async () => {
   return new Promise((resolve, reject) => {
